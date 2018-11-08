5d4c4d0f60220b19fa91c15bef5cc2c88893578a
a77321a <1817169988@qq.com>
2018-11-08 17:55:38 +0800

    测试窗口api

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tmis'
+  }
+</script>
+
+<style scoped>
+  /* CSS */
+  #app{
+    width:100%;
+    height:100%;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="wrapper">
+    <Menu mode="horizontal" theme="light" active-name="1">
+        <MenuItem name="1">
+            <Icon type="ios-home" />
+            首页
+        </MenuItem>
+        <MenuItem name="2">
+            <Icon type="ios-people" />
+            用户管理
+        </MenuItem>
+        <Submenu name="3">
+            <template slot="title">
+                <Icon type="ios-stats" />
+                统计分析
+            </template>
+            <MenuGroup title="使用">
+                <MenuItem name="3-1">新增和启动</MenuItem>
+                <MenuItem name="3-2">活跃分析</MenuItem>
+                <MenuItem name="3-3">时段分析</MenuItem>
+            </MenuGroup>
+            <MenuGroup title="留存">
+                <MenuItem name="3-4">用户留存</MenuItem>
+                <MenuItem name="3-5">流失用户</MenuItem>
+            </MenuGroup>
+        </Submenu>
+        <MenuItem name="4">
+            <Icon type="ios-construct" />
+            综合设置
+        </MenuItem>
+    </Menu>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
